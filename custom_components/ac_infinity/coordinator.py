18d15071d40315319b5b47fe1505d4ddcdea662a
Mike Thomas <6291808+mt0321@users.noreply.github.com>
2025-08-31 16:32:11 -0700

    Remove unused logic and clarify variable naming

--- custom_components/ac_infinity/coordinator.py
+++ custom_components/ac_infinity/coordinator.py
@@ -38,8 +38,7 @@ class ACInfinityDataUpdateCoordinator(ActiveBluetoothDataUpdateCoordinator[None]
         )
         self.ble_device = ble_device
         self.controller = controller
-        self._ready_event = asyncio.Event()
-        self._was_unavailable = True
+        self._device_ready = asyncio.Event()
 
     @callback
     def _needs_poll(
@@ -47,8 +46,6 @@ class ACInfinityDataUpdateCoordinator(ActiveBluetoothDataUpdateCoordinator[None]
         service_info: bluetooth.BluetoothServiceInfoBleak,
         seconds_since_last_poll: float | None,
     ) -> bool:
-        # Only poll if hass is running, we need to poll,
-        # and we actually have a way to connect to the device
         return (
             self.hass.state == CoreState.running
             and (seconds_since_last_poll is None or seconds_since_last_poll > 30)
@@ -66,14 +63,6 @@ class ACInfinityDataUpdateCoordinator(ActiveBluetoothDataUpdateCoordinator[None]
         await self.controller.update()
 
     @callback
-    def _async_handle_unavailable(
-        self, service_info: bluetooth.BluetoothServiceInfoBleak
-    ) -> None:
-        """Handle the device going unavailable."""
-        super()._async_handle_unavailable(service_info)
-        self._was_unavailable = True
-
-    @callback
     def _async_handle_bluetooth_event(
         self,
         service_info: bluetooth.BluetoothServiceInfoBleak,
@@ -85,17 +74,16 @@ class ACInfinityDataUpdateCoordinator(ActiveBluetoothDataUpdateCoordinator[None]
             service_info.device, service_info.advertisement
         )
         if self.controller.name:
-            self._ready_event.set()
+            self._device_ready.set()
         self.logger.debug(
             "%s: AC Infinity data: %s", self.ble_device.address, self.controller.state
         )
-        self._was_unavailable = False
         super()._async_handle_bluetooth_event(service_info, change)
 
     async def async_wait_ready(self) -> bool:
         """Wait for the device to be ready."""
         with contextlib.suppress(asyncio.TimeoutError):
             async with async_timeout.timeout(DEVICE_STARTUP_TIMEOUT):
-                await self._ready_event.wait()
+                await self._device_ready.wait()
                 return True
         return False
